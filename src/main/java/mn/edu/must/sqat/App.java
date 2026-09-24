fb76dd3970fda8f97e404614ef6003d92604cc73
B232270001 <mend2023@gmail.com>
2026-09-24 22:43:08 +0800

    Initialize Maven JUnit project

---
+++ src/main/java/mn/edu/must/sqat/App.java
@@ -0,0 +1,13 @@
+package mn.edu.must.sqat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
